6b0a0b72a801f5b7587e70b48442cbe9358226a1
thodsaphon <thodsaphon.t@entronica.co.th>
2019-11-05 22:05:18 +0700

    add ptsRule.

--- sourcecode/src/modules/getPtsAppProfileEntry/getPtsAppProfileEntry.ctrl.js
+++ sourcecode/src/modules/getPtsAppProfileEntry/getPtsAppProfileEntry.ctrl.js
@@ -53,46 +53,28 @@ exports.getPtsAppProfileEntry = function (req, res) {
     appLog.debug('raw headers : ', JSON.stringify(req.rawHeaders))
 
     const ret = {
-      resultDescription: 'Success',
-      ptsAppProfileEntry: [
+      "resultCode": "20000",
+      "resultDescription": "Success",
+      "ptsAppProfileEntry": [
         {
-          ptsRule: 'ewoJImNoZWNrTmV0d29yayI6IFsKCQl7CgkJCSJyZWdpc3RlckNoYW5uZWxBdmFpbGFiaWxpdHkiOiAib3RwIiwKCQkJIm5ldHdvcmtBdmFpbGFiaWxpdHkiOiBbCgkJCQkiT1RIRVIiCgkJCV0sCgkJCSJzZXJ2aWNlSWQiOiAicGxheWJveC14eHh4IiwKCQkJImFjY291bnRUeXBlIjogInhYeCIsCgkJCSJvdHBDaGFubmVsIjogInNtc05hamEiCgkJfSwKCQl7CgkJCSJyZWdpc3RlckNoYW5uZWxBdmFpbGFiaWxpdHkiOiAidXNlcnB3ZDNyZCIsCgkJCSJuZXR3b3JrQXZhaWxhYmlsaXR5IjogWwoJCQkJIkFJUyIKCQkJXQoJCX0sCgkJewoJCQkicmVnaXN0ZXJDaGFubmVsQXZhaWxhYmlsaXR5IjogInVzZXJwd2QiLAoJCQkibmV0d29ya0F2YWlsYWJpbGl0eSI6IFsKCQkJCSJBSVMiCgkJCV0KCQl9LAoJCXsKCQkJInJlZ2lzdGVyQ2hhbm5lbEF2YWlsYWJpbGl0eSI6ICJhdXRvIiwKCQkJIm5ldHdvcmtBdmFpbGFiaWxpdHkiOiBbCgkJCQkiQUlTRkJCIgoJCQldCgkJfQoJCSwKCQl7CgkJCSJyZWdpc3RlckNoYW5uZWxBdmFpbGFiaWxpdHkiOiAiZW1haWwiLAoJCQkibmV0d29ya0F2YWlsYWJpbGl0eSI6IFsKCQkJCSJBSVNGQkIiCgkJCV0KCQl9CgkJLAoJCXsKCQkJInJlZ2lzdGVyQ2hhbm5lbEF2YWlsYWJpbGl0eSI6ICJzb2NpYWwiLAoJCQkibmV0d29ya0F2YWlsYWJpbGl0eSI6IFsKCQkJCSJBSVNGQkIiCgkJCV0KCQl9CgldLAoJImF1dG9BY3JGbGFnIjogdHJ1ZSwKCSJjb25zZW50RmxhZyI6IGZhbHNlLAoJImNhY2hpbmdUaW1lIjogMTAsCgkiZmxvd1R5cGUiOiAibm9ybWFsIiwKCSJzYWx0IjogInh4eHh4IiwKCSJhdXRvT2J0YWluRmxhZyI6IHRydWUsCgkidGVtcGxhdGVOYW1lIjogImxvZ2luMiIsCgkiY2FsbEJhY2tVcmwiOiAiaHR0cDovL2dvb2dsZS5jb20iLAoJImNhbGxCYWNrTWV0aG9kIjogWwoJCSJQT1NUIgoJXSwKCSJyZWRpcmVjdFVSTCI6ICJodHRwOi8vMTAuMS4xMjAuMTozMC9rYmMuaW4udGgiLAogICAgInJlZGlyZWN0X3VyaSI6ICJodHRwczovL2Fpcy1zdGctYXBwLmRlbW8tdmlkbnQuY29tL3BvcnRhbC9hdXRoIiwKICAgICJjYXRlZ29yeU5vdEFsbG93IjpbImJ1c2luZXNzIiwiY29ycGVyYXRlIl0sCgkibG9naW5CMkIyQ0F0dGFjaEZsYWciIDpmYWxzZQp9IA=='
+          "dn":"ptsAppName=TestSingularity,ds=Service,ds=pts,subdata=services,uid=661550888279074,ds=SUBSCRIBER,o=AIS,dc=C-NTDB",
+          "ptsAppName":"TestSingularity",
+          "objectClass":"ptsAppProfileEntry",
+          /* 
+          * IOT P'Nui
+          */
+          ptsRule: 'ewoJImNoZWNrTmV0d29yayI6IFt7CgkJInJlZ2lzdGVyQ2hhbm5lbEF2YWlsYWJpbGl0eSI6ICJhdXRvIiwKCQkibmV0d29ya0F2YWlsYWJpbGl0eSI6IFsiQUlTIl0KCX0sIHsKCQkicmVnaXN0ZXJDaGFubmVsQXZhaWxhYmlsaXR5IjogIm90cCIsCgkJIm5ldHdvcmtBdmFpbGFiaWxpdHkiOiBbIkFJU1dJRkkiLCAiT1RIRVIiLCAiQUlTRkJCIl0sCgkJInNlcnZpY2VJZCI6ICJBSVNQbGF5IiwKCQkiYWNjb3VudFR5cGUiOiAiYWxsIiwKCQkib3RwQ2hhbm5lbCI6ICJzbXMiCgl9XSwKCSJjb25zZW50RmxhZyI6IGZhbHNlLAoJImF1dG9PYnRhaW5GbGFnIjogZmFsc2UsCgkiY2FjaGluZ1RpbWUiOiAiMzAiLAoJImNhbGxCYWNrVXJsIjogImh0dHBzOi8vYWlzLXN0Zy1hcHBhZG0uZGVtby12aWRudC5jb20vb3NzL2dsb2JhbF9hY3Rpb25zL2Fpc191c2VyX25vdGlmaWNhdGlvbi8iLAoJImZsb3dUeXBlIjogIm5vcm1hbCIsCgkibG9naW5CMkIyQ0F0dGFjaEZsYWciOiB0cnVlLAoJInRlbXBsYXRlTmFtZSI6ICJ2aW1taV9sb2dpbiIsCgkiY2FsbEJhY2tNZXRob2QiOiBbIlBPU1QiXQp9'
+         /* 
+         * P'Flim
+         */
+        // "ptsRule":"eyJwb2xpY3kiOnsidGVtcGxhdGVfZm9ybSI6InNpbmd1bGFyaXR5Iiwic3NvIjp0cnVlLCJjaGFubmVscyI6W3siZW1haWxfcGFzc3dvcmQiOnsiaGFuZGxlX3JlZGlyZWN0X2J5X3RlbXBsYXRlIjp0cnVlLCJhY3RpdmF0aW9uX2xpbmtfaG9zdCI6Imh0dHBzOi8vaW90LWF1dGh2My5haXMuY28udGgvYXV0aC8iLCJ0ZW1wbGF0ZV9pZF90b2tlbl9pbmZvIjp7ImZpcnN0bmFtZSI6InN1YnNjcmliZXJQcm9maWxlLmdpdmVuTmFtZSIsImxhc3RuYW1lIjoic3Vic2NyaWJlclByb2ZpbGUuc3VybmFtZSIsInVzZXJuYW1lIjoic3Vic2NyaWJlclByb2ZpbGUucGZVc2VybmFtZSJ9fX0seyJzb2NpYWxfZ29vZ2xlIjp7ImNsaWVudF9pZCI6Ijg4NzA1MDE4NTAxOS0wNTluNWlmMG8zMXVxOGpobm5iYXRwOTE2azc2NmVwci5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsImNsaWVudF9zZWNyZXQiOiJIVEZVOXFmVXdwYUFFMmhCX0VIYjJGVDIiLCJzZWxlY3RpdmVfcHVibGljX2lkIjoiZW1haWx8ZW1haWwsZ29vZ2xlX2lkfHN1YiIsInNjb3BlX3Blcm1pc3Npb24iOiJwcm9maWxlIG9wZW5pZCBlbWFpbCIsInRlbXBsYXRlX2lkX3Rva2VuX2luZm8iOnsiaWQiOiJzdWIiLCJlbWFpbCI6ImVtYWlsIiwibmFtZSI6Im5hbWUiLCJwaWN0dXJlIjoicGljdHVyZSJ9fX0seyJzb2NpYWxfZmIiOnsiYXBwX2lkIjoiMTEwNTEwNDg4NjMxNjc2NiIsImFwcF9hY2NjZXNzX3Rva2VuIjoiMTEwNTEwNDg4NjMxNjc2Nnx4S01kNVZuYmZnUUdQUGlKeVI0cE11Sm1yZ2siLCJzZWxlY3RpdmVfcHVibGljX2lkIjoiZW1haWx8ZW1haWwsZmJfaWR8aWQiLCJjaGFubmVsX3NlY3JldCI6Ijk3NjYzMTA0NTEwMTkxMDQ1NTM4NDA4M2U0NTZiNDhkIiwic2NvcGVfZ3JhcGgiOiJpZCxuYW1lLGVtYWlsLHBpY3R1cmUudHlwZShsYXJnZSksYmlydGhkYXksZ2VuZGVyLGZpcnN0X25hbWUsbGFzdF9uYW1lIiwic2NvcGVfcGVybWlzc2lvbiI6ImVtYWlsLHVzZXJfZ2VuZGVyLHVzZXJfYmlydGhkYXkiLCJ0ZW1wbGF0ZV9pZF90b2tlbl9pbmZvIjp7ImlkIjoiaWQiLCJlbWFpbCI6ImVtYWlsIiwibmFtZSI6Im5hbWUiLCJiaXJ0aGRheSI6ImJpcnRoZGF5IiwiZ2VuZGVyIjoiZ2VuZGVyIiwicGljdHVyZSI6InBpY3R1cmUuZGF0YS51cmwifX19LHsic29jaWFsX2xpbmUiOnsiY2hhbm5lbF9pZCI6IjE2MjE5NjYzNjciLCJjaGFubmVsX3NlY3JldCI6IjQwYWI0ZTY5MzllZTRlYjdkNTg0MDAxNzgwMmQ0N2IwIiwic2VsZWN0aXZlX3B1YmxpY19pZCI6ImVtYWlsfGVtYWlsLGxpbmVfaWR8c3ViIiwic2NvcGVfcGVybWlzc2lvbiI6InByb2ZpbGUgb3BlbmlkIGVtYWlsIiwidGVtcGxhdGVfaWRfdG9rZW5faW5mbyI6eyJpZCI6InN1YiIsImVtYWlsIjoiZW1haWwiLCJuYW1lIjoibmFtZSIsInBpY3R1cmUiOiJwaWN0dXJlIn19fV19fQ=="
+        /* 
+         * random scenario
+         */
+        // ptsRule: 'ewoJImNoZWNrTmV0d29yayI6IFt7CgkJInJlZ2lzdGVyQ2hhbm5lbEF2YWlsYWJpbGl0eSI6ICJhdXRvIiwKCQkibmV0d29ya0F2YWlsYWJpbGl0eSI6IFsiQUlTIl0KCX0sIHsKCQkicmVnaXN0ZXJDaGFubmVsQXZhaWxhYmlsaXR5IjogInVzZXJwd2QiLAoJCSJuZXR3b3JrQXZhaWxhYmlsaXR5IjogWyJBSVNXSUZJIiwgIk9USEVSIiwgIkFJU0ZCQiJdLAoJCSJzZXJ2aWNlSWQiOiAiQUlTUGxheSIsCgkJImFjY291bnRUeXBlIjogImFsbCIsCgkJIm90cENoYW5uZWwiOiAic21zIgoJfV0sCgkiY29uc2VudEZsYWciOiBmYWxzZSwKCSJhdXRvT2J0YWluRmxhZyI6IGZhbHNlLAoJImNhY2hpbmdUaW1lIjogIjMwIiwKCSJzYWx0IjogImZsaW0iLAoJImNhbGxCYWNrVXJsIjogImh0dHBzOi8vYWlzLXN0Zy1hcHBhZG0uZGVtby12aWRudC5jb20vb3NzL2dsb2JhbF9hY3Rpb25zL2Fpc191c2VyX25vdGlmaWNhdGlvbi8iLAoJImZsb3dUeXBlIjogIm5vcm1hbCIsCgkibG9naW5CMkIyQ0F0dGFjaEZsYWciOiB0cnVlLAoJInRlbXBsYXRlTmFtZSI6ICJ2aW1taV9sb2dpbiIsCgkiY2FsbEJhY2tNZXRob2QiOiBbIlBPU1QiXQp9'
         }
       ]
     }
-    // {
-    //   resultDescription: 'Success',
-		// 	"template_name": "vimmi_login",
-		// 	"redirectURL": "http://akdev.vidnt.com",
-		// 	"networkAvailability": ["AISWIFI", "OTHER", "AISFBB"],
-		// 	"registerChannelAvailability": ["otp"],
-		// 	"otpChannel": "sms",
-		// 	"accountType": "all",
-		// 	"serviceId": "AISPlay",
-		// 	"clientNetworkAvailability": "OTHER",
-		// 	"checkNetworkNoneValidate": [{
-		// 		"networkAvailability": ["AIS"],
-		// 		"registerChannelAvailability": "auto"
-		// 	}, {
-		// 		"otpChannel": "sms",
-		// 		"accountType": "all",
-		// 		"serviceId": "AISPlay",
-		// 		"networkAvailability": ["AISWIFI", "OTHER", "AISFBB"],
-		// 		"registerChannelAvailability": "otp"
-		// 	}],
-		// 	"callBackMethod": ["POST"],
-		// 	"loginB2B2CAttachFlag": true,
-		// 	"flowType": "normal",
-		// 	"callBackUrl": "http://appadm.ais-vidnt.com/oss/global_actions/ais_user_notification/",
-		// 	"autoObtainFlag": false,
-		// 	"consentFlag": false,
-		// 	"developerMessage": "Success",
-		// 	"resultCode": "20000",
-		// 	"partnerId": "30052",
-		// 	"appVersion": "1.2.0",
-		// 	"platformName": "Android",
-		// 	"appName": "vimmiottapp"
-		// }
     res.req = req
     appLog.stat(stat.retResSuc(conf.cmd))
     res.header('X-Session-Id', req.headers['x-session-id'])
